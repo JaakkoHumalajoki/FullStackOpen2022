f54fdbe7d1b351f3861bf402a9d13d6e66950b11
Jaakko Humalajoki <jaakko.humalajoki@gmail.com>
2022-11-30 14:36:47 +0200

    add totalLikes function and its testing

--- part4/bloglist/tests/list_helper.test.js
+++ part4/bloglist/tests/list_helper.test.js
@@ -1,9 +1,43 @@
-const { dummy } = require('../utils/list_helper')
+const { dummy, totalLikes } = require('../utils/list_helper')
 
 describe('dummy', () => {
   test('dummy returns 1', () => {
     const blogs = []
 
     expect(dummy(blogs)).toBe(1)
+  })
+})
+
+describe('totalLikes', () => {
+  const listOfThree = [
+    {
+      title: 'Moby Dick',
+      author: 'Hermal Melville',
+      url: 'https://en.wikipedia.org/wiki/Moby-Dick',
+      likes: 15,
+      id: '63874affcb36b797cd196001'
+    },
+    {
+      title: 'Don QuiXote',
+      author: 'Miguel de Cervantes',
+      url: 'https://en.wikipedia.org/wiki/Don_Quixote',
+      likes: 21,
+      id: '63874affcb36b797cd196002'
+    },
+    {
+      title: 'The Art of War',
+      author: 'Sun Tzu',
+      url: 'https://en.wikipedia.org/wiki/The_Art_of_War',
+      likes: 4,
+      id: '63874affcb36b797cd196003'
+    }
+  ]
+
+  test('totalLikes counts correct', () => {
+    expect(totalLikes(listOfThree)).toBe(40)
+  })
+
+  test('totalLikes returns zero on empty array', () => {
+    expect(totalLikes([])).toBe(0)
   })
 })
