205429bc5d5ea044dd2a07adf78f60f09ce75021
Jaakko Humalajoki <jaakko.humalajoki@gmail.com>
2022-11-30 14:50:58 +0200

    refactor tests

--- part4/bloglist/tests/list_helper.test.js
+++ part4/bloglist/tests/list_helper.test.js
@@ -1,5 +1,41 @@
 const { dummy, totalLikes } = require('../utils/list_helper')
 
+const emptyList = []
+
+const listOfOne = [
+  {
+    title: 'Moby Dick',
+    author: 'Hermal Melville',
+    url: 'https://en.wikipedia.org/wiki/Moby-Dick',
+    likes: 15,
+    id: '63874affcb36b797cd196001'
+  }
+]
+
+const listOfThree = [
+  {
+    title: 'Moby Dick',
+    author: 'Hermal Melville',
+    url: 'https://en.wikipedia.org/wiki/Moby-Dick',
+    likes: 15,
+    id: '63874affcb36b797cd196001'
+  },
+  {
+    title: 'Don QuiXote',
+    author: 'Miguel de Cervantes',
+    url: 'https://en.wikipedia.org/wiki/Don_Quixote',
+    likes: 21,
+    id: '63874affcb36b797cd196002'
+  },
+  {
+    title: 'The Art of War',
+    author: 'Sun Tzu',
+    url: 'https://en.wikipedia.org/wiki/The_Art_of_War',
+    likes: 4,
+    id: '63874affcb36b797cd196003'
+  }
+]
+
 describe('dummy', () => {
   test('dummy returns 1', () => {
     const blogs = []
@@ -9,35 +45,15 @@ describe('dummy', () => {
 })
 
 describe('totalLikes', () => {
-  const listOfThree = [
-    {
-      title: 'Moby Dick',
-      author: 'Hermal Melville',
-      url: 'https://en.wikipedia.org/wiki/Moby-Dick',
-      likes: 15,
-      id: '63874affcb36b797cd196001'
-    },
-    {
-      title: 'Don QuiXote',
-      author: 'Miguel de Cervantes',
-      url: 'https://en.wikipedia.org/wiki/Don_Quixote',
-      likes: 21,
-      id: '63874affcb36b797cd196002'
-    },
-    {
-      title: 'The Art of War',
-      author: 'Sun Tzu',
-      url: 'https://en.wikipedia.org/wiki/The_Art_of_War',
-      likes: 4,
-      id: '63874affcb36b797cd196003'
-    }
-  ]
-
   test('totalLikes counts correct', () => {
     expect(totalLikes(listOfThree)).toBe(40)
   })
 
+  test('totalLikes equals the likes of the only blog in array', () => {
+    expect(totalLikes(listOfOne)).toBe(15)
+  })
+
   test('totalLikes returns zero on empty array', () => {
-    expect(totalLikes([])).toBe(0)
+    expect(totalLikes(emptyList)).toBe(0)
   })
 })
