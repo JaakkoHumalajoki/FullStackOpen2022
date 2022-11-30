166d7c0e78f4cfbb659ba851bc6e232bbfd6de3d
Jaakko Humalajoki <jaakko.humalajoki@gmail.com>
2022-11-30 11:53:47 +0200

    clean up blogSchema toJSON method

--- part4/bloglist/models/blog.js
+++ part4/bloglist/models/blog.js
@@ -13,6 +13,16 @@ const blogSchema = new mongoose.Schema({
   }
 })
 
+blogSchema.set('toJSON', {
+  transform: (_doc, ret) => {
+    ret.id = ret._id
+    delete ret._id
+    delete ret.__v
+
+    return ret
+  }
+})
+
 const Blog = mongoose.model('Blog', blogSchema)
 
 module.exports = Blog
