a5cadece8c34706763b31bd48986c743ad74d8a1
Jaakko Humalajoki <jaakko.humalajoki@gmail.com>
2022-12-01 15:09:00 +0200

    disable logger during testing

--- part4/bloglist/utils/logger.js
+++ part4/bloglist/utils/logger.js
@@ -1,8 +1,12 @@
+const { NODE_ENV } = require('./config')
+
 const info = (...params) => {
+  if (NODE_ENV === 'test') return
   console.log(...params)
 }
 
 const error = (...params) => {
+  if (NODE_ENV === 'test') return
   console.error(...params)
 }
 
