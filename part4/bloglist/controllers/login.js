4ff3ee18a1bf1771b30c463d378bd36ada73f9a7
Jaakko Humalajoki <jaakko.humalajoki@gmail.com>
2022-12-14 17:32:43 +0200

    add token expiration time

--- part4/bloglist/controllers/login.js
+++ part4/bloglist/controllers/login.js
@@ -25,7 +25,7 @@ loginRouter.post('/', async (req, res) => {
     id: user._id
   }
 
-  const token = jwt.sign(userForToken, LOGIN_SECRET)
+  const token = jwt.sign(userForToken, LOGIN_SECRET, { expiresIn: 60 * 60 })
 
   return res
     .status(200)
--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -31,6 +31,9 @@ const errorHandler = (err, _req, res, next) => {
   if (err.name === 'JsonWebTokenError') {
     return res.status(401).json({ error: 'invalid token' })
   }
+  if (err.name === 'TokenExpiredError') {
+    return res.status(401).json({ error: 'token expired' })
+  }
 
   next(err)
 }
