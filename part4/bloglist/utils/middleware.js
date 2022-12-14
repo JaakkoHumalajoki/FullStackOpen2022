aaa89aa0570bc27f17020ab7b96d33177a2ed060
Jaakko Humalajoki <jaakko.humalajoki@gmail.com>
2022-12-14 17:28:52 +0200

    add token error handling

--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -28,6 +28,9 @@ const errorHandler = (err, _req, res, next) => {
   if (err.name === 'ValidationError') {
     return res.status(400).json({ error: err.message })
   }
+  if (err.name === 'JsonWebTokenError') {
+    return res.status(401).json({ error: 'invalid token' })
+  }
 
   next(err)
 }
